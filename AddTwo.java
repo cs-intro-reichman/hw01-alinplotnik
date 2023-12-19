fc7039744f3ac300656be2616de61de54524e13e
alinplotnik <alin.plotnikov@runi.ac.il>
2023-12-19 17:24:26 +0200

    Add awesome code for Homework 1

--- AddTwo.java
+++ AddTwo.java
@@ -1,18 +1,11 @@
-/*
- * Adds two given integers and prints the result in a fancy way.
- */
 public class AddTwo {
-	public static void main(String[] args) {
-		
+    public static void main(String[] args) {
+	
 		int a = Integer.parseInt(args[0]);
 		
 		int b = Integer.parseInt(args[1]);
 		
-		System.out.println("*********                 *********");
-		System.out.println("**********                 **********");
-		System.out.println(" TADAAAA " + (a + b));
-		System.out.println("***********               *************");
-		System.out.println("*************                 *************");
-		System.out.println("******************          ******************");	
+		System.out.println(a + " " + "+" + " " + b + " " + "=" + " " + (a+b));
+	
 	}
 }
