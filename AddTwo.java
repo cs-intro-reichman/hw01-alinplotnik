f5b6f0c270053e315d2621feb545d6320f77b345
alinplotnik <alin.plotnikov@runi.ac.il>
2023-12-19 14:40:40 +0200

    Add awesome code for Homework 1

--- AddTwo.java
+++ AddTwo.java
@@ -3,6 +3,16 @@
  */
 public class AddTwo {
 	public static void main(String[] args) {
-		// Put your code here	
+		
+		int a = Integer.parseInt(args[0]);
+		
+		int b = Integer.parseInt(args[1]);
+		
+		System.out.println("*********                 *********");
+		System.out.println("**********                 **********");
+		System.out.println(" TADAAAA " + (a + b));
+		System.out.println("***********               *************");
+		System.out.println("*************                 *************");
+		System.out.println("******************          ******************");	
 	}
 }
