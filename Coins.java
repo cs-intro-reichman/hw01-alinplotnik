b647b8d4e57bc659b64d2fe2e8983bc4819e794c
alinplotnik <alin.plotnikov@runi.ac.il>
2023-12-20 12:29:57 +0200

    Add awesome code for Homework 1

--- Coins.java
+++ Coins.java
@@ -1,9 +1,11 @@
-/*
- * Write a program that gets a quantity of cents as a command-line argument.
- * The program prints how to represent this quantity using as many quarters as possible, plus the remainder in cents.
- */
 public class Coins {
 	public static void main(String[] args) {
-		// Put your code here
+		
+		int coins_num = Integer.parseInt(args[0]);
+		int cents_num = (coins_num % 25);
+		int quarters_num = ((coins_num - cents_num)/25);
+
+		System.out.println("Use " + quarters_num + " quarters and " + cents_num + " cents" );
+		
 	}
 }
