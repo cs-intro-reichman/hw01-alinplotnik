3aa4873718d979fef3a63bdffc1928ca98be5b76
alinplotnik <alin.plotnikov@runi.ac.il>
2023-12-21 10:15:06 +0200

    Add awesome code for Homework 1

--- LinearEq.java
+++ LinearEq.java
@@ -5,5 +5,15 @@
  * Treats the three arguments as well as the computed value as double values
  */
 public class LinearEq {
+	public static void main(String[] args) {
 	// Put your code here
+	Double a = (double)Integer.parseInt(args[0]);
+	Double b = (double)Integer.parseInt(args[1]);
+	Double c = (double)Integer.parseInt(args[2]);
+	Double x = ((c-b)/a);
+	
+	System.out.println(a + " * x + " + b + " = " + c);
+	System.out.println("x = " + x);
+	
+	}
 }
